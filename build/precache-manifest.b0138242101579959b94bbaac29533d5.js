b91a78ae31f4b9a5f2aeed29ad85c46ad2eb20e9
ibraheemz <ibrahemrwash18@gmail.com>
2023-06-07 22:42:49 +0300

    integrated catch YT &spotify info functions - inprogress -

---
+++ build/precache-manifest.b0138242101579959b94bbaac29533d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "40abc47aa69ae986a458",
+    "url": "/Listit/static/css/main.6ecde167.chunk.css"
+  },
+  {
+    "revision": "40abc47aa69ae986a458",
+    "url": "/Listit/static/js/main.40abc47a.chunk.js"
+  },
+  {
+    "revision": "b2decc115e52433e49fc",
+    "url": "/Listit/static/css/1.c8742fe7.chunk.css"
+  },
+  {
+    "revision": "b2decc115e52433e49fc",
+    "url": "/Listit/static/js/1.b2decc11.chunk.js"
+  },
+  {
+    "revision": "626709a7f26cce98577d",
+    "url": "/Listit/static/js/runtime~main.626709a7.js"
+  },
+  {
+    "revision": "38f6635bca8401b6c8199a5287a2a227",
+    "url": "/Listit/static/media/Background2.38f6635b.jpg"
+  },
+  {
+    "revision": "645cb531d0e5330fa468dade64fdd880",
+    "url": "/Listit/static/media/Background.645cb531.jpg"
+  },
+  {
+    "revision": "50315cbf788947a101f14ad1edcf6c4b",
+    "url": "/Listit/index.html"
+  }
+];
