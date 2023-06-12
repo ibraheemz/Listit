ac84fcd5ba47ea047b8b88dc67bd8c8da9e117d5
ibraheemz <ibrahemrwash18@gmail.com>
2023-06-12 16:01:11 +0300

    backend api calls for client id and secret created, call to add songs to playlist

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,29 +2,24 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 const Header = () => {
-    const CLIENT_ID = "71749fb665a1459694d28cfde5964580"
-    const REDIRECT_URI = "http://localhost:3000"
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-    const RESPONSE_TYPE = "token"
     
     const [token, setToken] = useState("")
 
     useEffect(() => {
-        const hash = window.location.hash
+        const pathname = window.location.pathname
         let token = window.localStorage.getItem("token")
 
-        if (!token && hash) {
-            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-
-            window.location.hash = ""
+        if (!token && pathname[1] === "a") {
+            token = pathname.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+            window.location.pathname = ""
             window.localStorage.setItem("token", token)
+        } else {
+            token = window.localStorage.token
         }
 
         setToken(token)
 
     }, [])
-
-
 
     const logout = () => {
         setToken("")
@@ -43,7 +38,7 @@ const Header = () => {
                 </ul>
                 {
                     !token
-                    ? <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`} className='login-button'>Login with Spotify</a>
+                    ? <a href="http://localhost:8888/login" className='login-button'>Login with Spotify</a>
                     : <button className='login-button' onClick={logout}>Log out</button>
                         
                 }
--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,50 +1,104 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Alert from "./Alert";
 
 const Intro = () => {
     const [playlistLink, setPlaylistLink] = useState("");
-    const token = window.localStorage.getItem("token");
-    var playlistSongsInfo = [];
-    var spotifyUserId = "";
+    const [googleToken, setGoogleToken] = useState("")
+    const [spotifyUserId, setSpotifyUserId] = useState("")
+    const [spotifyPlaylistId, setSpotifyPlaylistId] = useState("")
+    const [playlistTracksnames, setPlaylistTracksnames] = useState([])
+    const [tracksUri, setTracksUri] = useState([])
+    var token = window.localStorage.getItem("token");
+
+    useEffect(() => {
+        axios.get("http://localhost:8888/googleToken").then((response) => {
+            setGoogleToken(response.data)
+        })
+    },[])
+
+    useEffect(() => {
+        spotifyUserId.length &&
+        createSpotifyPlayList()
+    },[spotifyUserId])
+    //once we get playlistTracksnames, getTracksUri
+    useEffect(() => {
+        playlistTracksnames &&
+        getTracksUri()
+    },[playlistTracksnames, spotifyPlaylistId])
+    //once we get tracksUri, add Tracks to spotify playlist
+    useEffect(() => {
+        //tracksUri.length &&
+        spotifyPlaylistId &&
+        tracksUri &&
+        addTracksToList()
+    },[tracksUri])
 
     const handleInput = (e) => {
         setPlaylistLink(e.target.value)
     }
     //get Youtube playlist id
     const handleLink = (e) => {
+
+        //check if the user input is a valid link if not, alert and disable convert button
+        function isYoutubePlaylistLink(url) {
+            const regex = /&list=([a-zA-Z0-9_-]{18})/;
+            return regex.test(url);
+        }
+        if(!isYoutubePlaylistLink(playlistLink)) {
+            window.alert('Please Make sure This Link is a YouTube playlist link.')
+            return;
+        }
+          
+        //check if user is logged in and token is stored in localStorage
+        if(!window.localStorage.token) {
+            window.alert("please login with your spotify account")
+        } else {
+            const playListId = playlistLink.slice(playlistLink.indexOf("list=") + 5, playlistLink.indexOf("list=") + 39)
+            console.log("Play List Id: ", playListId);
+            e.preventDefault()
+            getSpotifyUserId()
+            getPlaylistInfo(playListId)
+        }
         //slice the link from list= to 34 chars after
         //the playlist's id  string length is 34
-        const playListId = playlistLink.slice(playlistLink.indexOf("list=") + 5, playlistLink.indexOf("list=") + 39)
-        console.log("Play List Id: ", playListId);
-        e.preventDefault()
-
-        getPlaylistInfo(playListId)
-        getSpotifyUserId()
-        createSpotifyPlayList()
     }
+
+    const refreshToken = () => {
+        axios.get("http://localhost:8888/refresh_token").then((response) => {
+            token = response.access_token
+            console.log("refreshToken is set: ", response.access_token)
+        })
+    }
+
     //get songs info from Youtube playlist link and set it as an array for playlistSongsInfo
     const getPlaylistInfo = (playListId) => {
         const url = "https://www.googleapis.com/youtube/v3/playlistItems"
+        
         const params = {
-            key: process.env.REACT_APP_API_KEY,
+            key: googleToken,
             part: "snippet",
             playlistId: playListId,
             maxResults: 50
         }
+        
          axios.get(url, { params })
             .then(response => {
                 if(response.status === 200) {
-                    response.data.items.map((song) => {
+                    let playlistSongsInfo = [];
+                    response.data.items.forEach((song) => {
                         playlistSongsInfo.push(song.snippet.title)
                     })
+                    setPlaylistTracksnames(playlistSongsInfo)
+                    console.log("Youtube play list tracks info done")
                 } else {
                     console.log("getPlaylistInfo call wasn't successful: ", response.error)
                 }
+            }).catch((error) => {
+                console.log(error)
             })
 
     }
-
-
     //Get Spotify User Id  || Still no check if logged in #!!!!###
     const getSpotifyUserId = () => {
         
@@ -57,36 +111,95 @@ const Intro = () => {
         })
             .then(response => {
                 if(response.status === 200) {
-                    spotifyUserId = response.data.id
-                } else {
+                    setSpotifyUserId(response.data.id)
+                } else if(response.data.error.message === 'The access token expired'){
+                    refreshToken()
                     console.log("getSpotifyUserId call wasn't successful: ", response.error)
                 }
+            }).catch((error) => {
+                console.log(error)
             })
     }
     //Make a new playlist on user's Spotify Account
     const createSpotifyPlayList = () => {
-        const url = `https://api.spotify.com/v1/users/${spotifyUserId}/playlists`
-        const playlistDate = {
-            name: "LISTIT",
-            descriptipn: "Your Youtube Playlist is here now",
-            public: false,
-            collaborative: false
-        }
-        spotifyUserId 
-            ?axios.post(url, playlistDate, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    'Content-Type': 'application/json'
-                }
+        const axiosOptions = {
+            method: 'POST',
+            url: `https://api.spotify.com/v1/users/${spotifyUserId}/playlists`,
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/json',
+            },
+            data: {
+              name: 'LISTIT',
+              description: 'Your Youtube Playlist',
+              public: false,
+              collaborative: false
+            },
+          };    
+        axios(axiosOptions).then(response => {
+            if(response.data) {
+                console.log("createSpotifyPlayList call was successful: ", response.data)
+                setSpotifyPlaylistId(response.data.id)
+            } else {
+                console.log("createSpotifyPlayList call wasn't successful: ", response.error.message)
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
 
-            }).then(response => {
-                if(response.status === 200) {
-                    console.log("createSpotifyPlayList call was successful: ", response.data)
-                } else {
-                    console.log("createSpotifyPlayList call wasn't successful: ", response.error.message)
-                }
+    //get Tracks URIs
+    const getTracksUri = () => {
+        const url = "https://api.spotify.com/v1/search"
+        const headers = {
+            Authorization: `Bearer ${token}`
+        }
+        let uriArray = []
+        playlistTracksnames.forEach((track) => {
+            const params = {
+                type: "track",
+                limit: 1,
+                q: track
+            }
+            axios.get(url, { 
+                params: params,
+                headers: headers
+            }).then((response) => {
+                uriArray.push(response.data.tracks.items[0].uri)
+            }).catch((error) => {
+                console.log(error)
             })
-            :console.log("spotifyUserId is not set yet")
+        })
+        setTracksUri(uriArray)
+        console.log("Spotify URIs done, length is: ", uriArray.length)
+        
+    }
+
+    //add tracks to spotify playlist 
+    const addTracksToList = () => {
+        const axiosOptions = {
+            method: 'POST',
+            url: `https://api.spotify.com/v1/playlists/${spotifyPlaylistId}/tracks`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            data: {
+                "uris": tracksUri,
+                "position": 0
+            }
+            
+        }
+        console.log(tracksUri)
+        axios(axiosOptions).then((response) => {
+            if(response.status === 201)  {
+                console.log("addTracksToList func successfully done and response status is: ", response.status)
+                return (
+                    <Alert />
+                )
+            }
+        }).catch((error) => {
+            console.log("addTracksToList func wasn't successfully done: ", error)
+        })
     }
 
     const showModal = () => {
